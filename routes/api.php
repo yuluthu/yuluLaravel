69d2e1856b07e63458b50b7e1e96adcf754a2034
Matt Smith <Matt@yuluthu.co.uk>
2026-02-12 23:54:01 +0000

    creating controllers and setting up env vars

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClubController;
+use App\Http\Controllers\TournamentController;
+ 
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'club' => ClubController::class,
+    'tournament' => TournamentController::class,
+]);
